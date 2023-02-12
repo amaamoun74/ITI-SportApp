201795c78556821707819d9f2bbf10d64ab2c164
ahmed maamoun <amaamoun74@gmail.com>
2023-02-12 13:07:53 +0200

    update ui

--- SportApp/SportApp/View/Controller/SportsController/SportsViewController.swift
+++ SportApp/SportApp/View/Controller/SportsController/SportsViewController.swift
@@ -30,29 +30,35 @@ class ViewController: UIViewController, UICollectionViewDelegate , UICollectionV
 }
 
 extension ViewController {
-     func numberOfSections(in collectionView: UICollectionView) -> Int {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-
-     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-         return sportArray.count
-    }
-
     
-     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return sportArray.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell:SportsCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! SportsCollectionViewCell
         
-        
-         cell.SportName.text = sportArray[indexPath.row].sportTitle
-         cell.cellImage.image = UIImage(named: sportArray[indexPath.row].sportImage)
-        
+        cell.layer.masksToBounds = true
+        cell.layer.cornerRadius = 15
+        cell.SportName.text = sportArray[indexPath.row].sportTitle
+        cell.cellImage.image = UIImage(named: sportArray[indexPath.row].sportImage)
+        cell.sportImageFrame.layer.masksToBounds = true
+        cell.sportImageFrame.layer.cornerRadius = 30
+        // cell.sendSubviewToBack(cell.sportImageFrame)
+        //cell.bringSubviewToFront(cell.cellImage)
+        cell.insertSubview(cell.sportImageFrame, belowSubview: cell.cellImage)
+        cell.insertSubview(cell.cellImage, aboveSubview: cell.sportImageFrame)
         //let url = URL(string: imageList[indexPath.row])
         //cell.iv_image?.kf.setImage(with: url)
-       
+        
         return cell
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if sportArray[indexPath.row].sportImage == "football" || sportArray[indexPath.row].sportImage == "basketball" || sportArray[indexPath.row].sportImage == "tennis" || sportArray[indexPath.row].sportImage == "cricket"
         {
@@ -63,7 +69,7 @@ extension ViewController {
         {
             showSnakbar(msg: "sorry can't open \(sportArray[indexPath.row].sportTitle)")
         }
-     
+        
     }
     private func showSnakbar(msg : String){
         let snackbar = TTGSnackbar(
@@ -80,11 +86,18 @@ extension ViewController {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "AllLeagues")
         navigationController?.pushViewController(vc!, animated: true)
     }
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: IndexPath) -> UICollectionReusableView {
+        
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Sports", for: indexPath )
+        
+        headerView.backgroundColor = UIColor.blue
+        return headerView
+    }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (UIScreen.main.bounds.size.width/2.1), height: (UIScreen.main.bounds.size.width/3))
+        return CGSize(width: (UIScreen.main.bounds.size.width/2.1), height: (UIScreen.main.bounds.size.width/2) - 10)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
       return 15 // Keep whatever fits for you
